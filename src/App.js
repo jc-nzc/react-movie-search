052f036ca34637cfe81ef3c3118084dc42b217da
JC <node.zerocool@gmail.com>
2020-08-30 11:45:07 -0500

    new react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World, My Name is Jorge
+        </p>
+        <a
+          className="App-link"
+          href="https://jorge-cano.github.io/Background/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Portfolio
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
